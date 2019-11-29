5c3d92f55f2abe0d28968c71298da6b51d71b5ad
Marco Ferrati <marco.ferrati@gmail.com>
2019-11-29 10:46:06 +0100

    Segnata l'implementazione dell'interfaccia in App.java

--- src/main/java/it/unipd/tos/App.java
+++ src/main/java/it/unipd/tos/App.java
@@ -4,14 +4,21 @@
 
 package it.unipd.tos;
 
+import it.unipd.tos.business.exception.TakeAwayBillException;
+import it.unipd.tos.business.model.MenuItem;
+
+import java.util.List;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App implements TakeAwayBill {
+    @Override
+    public double getOrderPrice(List<MenuItem> items_ordered) throws TakeAwayBillException {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
