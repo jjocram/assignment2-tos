ee6867606f6aa5f043efcaf3bcda53c024e3726e
Marco Ferrati <marco.ferrati@gmail.com>
2019-11-30 17:15:06 +0100

    #3 Risolto problema con l'applicazione di entrambi gli sconti

--- src/main/java/it/unipd/tos/App.java
+++ src/main/java/it/unipd/tos/App.java
@@ -18,11 +18,20 @@ public class App implements TakeAwayBill {
     public double getOrderPrice(List<MenuItem> items_ordered) throws TakeAwayBillException {
         double conto_totale = items_ordered.stream().mapToDouble(MenuItem::getPrice).reduce(0, Double::sum);
 
+        double conto_solo_panini_e_fritti = items_ordered.stream()
+                .filter(e -> e.getItemType() == ItemType.PANINO || e.getItemType() == ItemType.FRITTO)
+                .mapToDouble(MenuItem::getPrice)
+                .reduce(0, Double::sum);
+
         long numero_di_panini = items_ordered.stream().filter(e -> e.getItemType() == ItemType.PANINO).count();
 
         if (numero_di_panini > 5){
-            double costo_panino_meno_costoso = items_ordered.stream().mapToDouble(MenuItem::getPrice).min().getAsDouble();
+            double costo_panino_meno_costoso = items_ordered.stream().filter(e -> e.getItemType() == ItemType.PANINO).mapToDouble(MenuItem::getPrice).min().getAsDouble();
             conto_totale = conto_totale - (0.5 * costo_panino_meno_costoso);
+        }
+
+        if (conto_solo_panini_e_fritti > 50.0) {
+            conto_totale = conto_totale - (0.1 * conto_totale);
         }
 
         return conto_totale;
