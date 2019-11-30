3685708118ea0a5c3d8a2b05cfd6f863c9701bc4
Marco Ferrati <marco.ferrati@gmail.com>
2019-11-30 15:10:18 +0100

    #2 Creato il test per il controllo dello sconto

--- src/test/java/it/unipd/tos/AppTest.java
+++ src/test/java/it/unipd/tos/AppTest.java
@@ -63,4 +63,25 @@ public class AppTest {
         items = null;
         result = app.getOrderPrice(items);
     }
+
+    @Test
+    public void testApplicazioneScontoSulPaninoMenoCaroSeNeVengonoCompartiPiuDiCinque(){
+        items = new ArrayList<>();
+        result = 0.0;
+
+        items.add(panino_vegetariano);
+        items.add(panino_vegetariano);
+        items.add(panino_vegetariano);
+        items.add(panino_vegetariano);
+        items.add(panino_primavera);
+        items.add(panino_primavera);
+
+        try {
+            result = app.getOrderPrice(items);
+        } catch (TakeAwayBillException e) {
+            e.printStackTrace();
+        }
+
+        Assert.assertEquals(32.25, result, 0.0D);
+    }
 }
